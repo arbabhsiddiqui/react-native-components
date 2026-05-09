72fc012c69c3631d27c4d714cda41b9d3b6f9293
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2026-05-09 12:49:55 +0530

    scroll view

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,46 +1,17 @@
-import { useState } from 'react'
-import { Image, StyleSheet, Text, View, TextInput, Pressable } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const HomeScreen = () => {
-  const [name, setName] = useState("")
+
+  const items = Array.from({ length: 200 }, (_, i) => `Item ${i + 1}`)
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <ScrollView>
+      {items.map(item => (
+        <Text key={item}>{item}</Text>
+      ))}
 
-      <Text numberOfLines={3}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi deleniti blanditiis mollitia enim ad minima corrupti eos iste id voluptatibus! Odio accusantium ex ipsam unde. Similique quaerat rem veniam incidunt!</Text>
-
-      <Image source={{ uri: 'https://chaicode.com/assets/hitesh-piyush-dark-fpMoT6Mp.webp' }}
-        width={200}
-        height={200}
-      />
-
-      <Image source={require('@/assets/images/icon.png')} style={{ height: 30, width: 30 }} />
-
-      <TextInput placeholderTextColor={"green"} placeholder='enter name' value={name} onChangeText={setName} style={{
-        borderWidth: 1,
-        borderColor: '#000',
-        marginTop: 30,
-        fontSize: 24
-      }} />
-
-      <Pressable onPress={() => alert('button press')} style={
-        ({ pressed }) => ({
-          backgroundColor: pressed ? '#4a42d4' : "#6c63ff"
-        })
-
-
-
-      }
-        hitSlop={{
-          top: 10,
-          left: 10,
-          bottom: 10,
-          right: 10
-        }}
-      >
-        {({ pressed }) => pressed ? <Text>pressing....</Text> : <Text>press me</Text>}
-      </Pressable>
-    </View>
+    </ScrollView>
   )
 }
 
