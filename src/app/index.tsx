805539db00625d48ad432bf0f166fcdf93a26a11
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2026-05-09 12:19:41 +0530

    image tag basic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
       <Text numberOfLines={3}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi deleniti blanditiis mollitia enim ad minima corrupti eos iste id voluptatibus! Odio accusantium ex ipsam unde. Similique quaerat rem veniam incidunt!</Text>
+      <Image source={{ uri: 'https://chaicode.com/assets/hitesh-piyush-dark-fpMoT6Mp.webp' }}
+        width={200}
+        height={200}
+      />
     </View>
   )
 }
