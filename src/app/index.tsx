ccb5472857cb293231e3751c72fb52f1fc3b9b28
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2026-05-10 11:43:03 +0530

    useSafeAreaInsets hook

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,36 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-
-const UnSafeScreen = () => {
-  return (<View style={{ flex: 1, backgroundColor: "#1c1c1e" }}>
-    <Text style={{ color: "#fff", fontSize: 18, padding: 16 }}>
-      Header (bleeds under notch!)
-    </Text>
-    <Text style={{ color: "#aaa", padding: 16 }}>
-      This content might be hidden behind the status bar in dark mode.
-    </Text>
-  </View>)
-}
-
-const SafeScreen = () => {
-  return (
-    <SafeAreaView edges={["top", "bottom"]} style={{ flex: 1, backgroundColor: "#1c1c1e" }}>
-      <Text style={{ color: "#fff", fontSize: 18, padding: 16 }}>
-        Header (bleeds under notch!)
-      </Text>
-      <Text style={{ color: "#aaa", padding: 16 }}>
-        This content might be is in save area bar in dark mode.
-      </Text>
-    </SafeAreaView>)
-}
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const index = () => {
+
+  const insets = useSafeAreaInsets()
+
   return (
-    <>
-      <SafeScreen />
-      {/* <UnSafeScreen /> */}
-    </>
+    <View style={{ flex: 1, marginBlockStart: insets.top, marginBlockEnd: insets.bottom }}>
+      <Text>index</Text>
+    </View>
   )
 }
 
