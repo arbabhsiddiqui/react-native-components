b623e03b0f6d7f657e72e4ba76e85173efbd0bc8
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2026-05-10 12:06:21 +0530

    style sheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,35 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
+
+
 
 const index = () => {
-
-  const insets = useSafeAreaInsets()
-
   return (
-    <View style={{ flex: 1, backgroundColor: 'red', marginBlockStart: insets.top, marginBlockEnd: insets.bottom }}>
-      <Text>index</Text>
-      <StatusBar barStyle={"dark-content"} />
-    </View>
+    <SafeAreaView>
+      <StatusBar style='dark' />
+      <View style={styles.card}>
+        <Text style={styles.title}>index</Text>
+        <Text style={styles.subTitle}>Hello</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    padding: 20,
+    margin: 16,
+    elevation: 6,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 8
+  },
+  title: {},
+  subTitle: {}
+})
